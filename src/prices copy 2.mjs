e34cbb9be2b98258e669de7562328dce9b3cec3f
Virva <virva.svala@gmail.com>
2022-02-07 15:33:26 +0200

    change1

--- src/prices copy 2.mjs
+++ src/prices copy 2.mjs
@@ -20,6 +20,7 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
+    const date2= req.query.date && Temporal.PlainDate.from(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
